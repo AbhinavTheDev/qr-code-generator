e1efbab67814f2372ecc04f9a5b63268898671ac
Abhinav <85792055+AbhinavTheDev@users.noreply.github.com>
2024-05-27 14:31:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const button = document.getElementById("generate");
 const input = document.getElementById("url");
-const FirstColor = document.getElementById("FirstColor");
-const SecondColor = document.getElementById("SecondColor");
+// const FirstColor = document.getElementById("FirstColor");
+// const SecondColor = document.getElementById("SecondColor");
 const qrdiv = document.getElementById("QR");
+const size = document.getElementById("size");
+const button2 = document.getElementById("buttons");
 
 const submitting = (e) => {
   e.preventDefault();
@@ -14,14 +16,19 @@ const submitting = (e) => {
     alert("Please enter URL !!!");
   } else {
     generateQRCode();
+    const saveUrl = qrdiv.querySelector("canvas").toDataURL();
+    // Create save button
+    createSaveBtn(saveUrl);
   }
 };
 // function for generating QR Code
 const generateQRCode = () => {
   var qrcode = new QRCode(document.getElementById("QR"), {
-    text: input.value,
-    colorDark: SecondColor.value,
-    colorLight: FirstColor.value,
+    text: input.value.trim(),
+    width: size.value,
+    height: size.value,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H,
   });
 };
@@ -31,4 +38,16 @@ const clearUI = () => {
   qrdiv.innerHTML = "";
 };
 
-button.addEventListener("click",submitting);
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.innerHTML = "Save Image";
+
+  link.href = saveUrl;
+  link.download = "qrcode.png";
+
+  document.getElementById("generated").appendChild(link);
+};
+
+button.addEventListener("click", submitting);
