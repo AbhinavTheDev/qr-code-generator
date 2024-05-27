016963307dace3067e5ef00c1d41cdf511938495
Abhinav <85792055+AbhinavTheDev@users.noreply.github.com>
2024-05-27 07:26:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,29 +32,3 @@ const clearUI = () => {
 };
 
 button.addEventListener("click",submitting);
-
-
-function domReady(fn) {
-    if (
-        document.readyState === "complete" ||
-        document.readyState === "interactive"
-    ) {
-        setTimeout(fn, 1000);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}
- 
-domReady(function () {
- 
-    // If found you qr code
-    function onScanSuccess(decodeText, decodeResult) {
-        alert("You Qr is : " + decodeText, decodeResult);
-    }
- 
-    let htmlscanner = new Html5QrcodeScanner(
-        "my-qr-reader",
-        { fps: 10, qrbos: 250 }
-    );
-    htmlscanner.render(onScanSuccess);
-});
