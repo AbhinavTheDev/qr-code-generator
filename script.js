3d0df155fcc37b0196820123812528adc11bfbd8
Abhinav <85792055+AbhinavTheDev@users.noreply.github.com>
2024-05-30 13:07:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const button = document.getElementById("generate");
 const input = document.getElementById("url");
-// const FirstColor = document.getElementById("FirstColor");
-// const SecondColor = document.getElementById("SecondColor");
 const qrdiv = document.getElementById("QR");
 const size = document.getElementById("size");
 const button2 = document.getElementById("buttons");
+let saveUrl = null;
 
 const submitting = (e) => {
   e.preventDefault();
@@ -16,9 +15,17 @@ const submitting = (e) => {
     alert("Please enter URL !!!");
   } else {
     generateQRCode();
-    const saveUrl = qrdiv.querySelector("canvas").toDataURL();
-    // Create save button
-    createSaveBtn(saveUrl);
+    SaveBtn();
+    const qrCanvas = qrdiv.querySelector("canvas");
+    qrCanvas.toBlob((blob) => {
+      if (saveUrl) {
+        URL.revokeObjectURL(saveUrl);
+      }
+      saveUrl = URL.createObjectURL(blob);
+      console.log(saveUrl);
+      // Create save button
+      SaveBtnLink(saveUrl);
+    });
   }
 };
 // function for generating QR Code
@@ -39,15 +46,42 @@ const clearUI = () => {
 };
 
 // Create save button to download QR code as image
-const createSaveBtn = (saveUrl) => {
+const SaveBtn = () => {
+  // remove existed div tags of save button
+  let existedBtn = button2.getElementsByTagName("div");
+  // console.log(existedBtn);
+  // exixtedBtn yha pr Nodelist de rha hai
+  //isliye index jaruri hai node remove
+  //karne ke liye isliye niche index 0 lagaya hai
+  while (existedBtn.length > 0) {
+    button2.removeChild(existedBtn[0]);
+  }
+  // button ka div generate kra hai
+  const buttonback = document.createElement("div");
+  buttonback.id = "generated";
+  buttonback.style.border = "1px solid #000";
+  buttonback.style.backgroundColor = "#ff853e";
+
+  button2.appendChild(buttonback);
+};
+
+const SaveBtnLink = (saveUrl) => {
+  // selecting parent tag of save button
+  let linkparent = document.getElementById("generated");
+
+  // remove existed anchor tags
+  let existedanchor = linkparent.getElementsByTagName("a");
+  while (existedanchor.length > 0) {
+    linkparent.removeChild(existedanchor);
+  }
+  // button pr link add kra hai
   const link = document.createElement("a");
   link.id = "save-link";
   link.innerHTML = "Save Image";
-
   link.href = saveUrl;
   link.download = "qrcode.png";
 
-  document.getElementById("generated").appendChild(link);
+  linkparent.appendChild(link);
 };
 
 button.addEventListener("click", submitting);
