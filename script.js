8e35ef80aa19d4eb111db7ff615491f1e08662e7
abhinavmittal554 <abhinavmittal554@gmail.com>
2024-08-11 22:25:44 +0000

    "Initial Commit"

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const button = document.getElementById("generate");
+const Gnbtn = document.getElementById("generateBtn");
 const input = document.getElementById("url");
 const qrdiv = document.getElementById("QR");
-const size = document.getElementById("size");
+const qrimg = document.querySelector("img");
+const color = document.getElementById("colorInput");
 const button2 = document.getElementById("buttons");
 const toggle = document.getElementById("toggle");
+const error = document.getElementById("error");
 let saveUrl = null;
 
 const submitting = (e) => {
   e.preventDefault();
 
   clearUI(); // clearing screen
-
-  // validation of url
-  if (input.value === "") {
-    alert("Please enter URL !!!");
+  if (!input.value.startsWith("https://")) {
+    // this condition check if input starts with https:// or not
+    error.innerHTML = "Invalid URL!!";
   } else {
+    // validation of url
     generateQRCode();
     SaveBtn();
     const qrCanvas = qrdiv.querySelector("canvas");
@@ -33,9 +35,9 @@ const submitting = (e) => {
 const generateQRCode = () => {
   var qrcode = new QRCode(document.getElementById("QR"), {
     text: input.value.trim(),
-    width: size.value,
-    height: size.value,
-    colorDark: "#000000",
+    width: 200,
+    height: 200,
+    colorDark: color.value,
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H,
   });
@@ -59,7 +61,7 @@ const SaveBtn = () => {
   }
   // Save button ka button tag generate kra hai
   const buttonback = document.createElement("button");
-  buttonback.id = "generated";
+  buttonback.id = "SaveBtn";
   buttonback.className = "SaveButton";
   buttonback.style.border = "1px solid #000";
   buttonback.style.backgroundColor = "#ff853e";
@@ -69,7 +71,7 @@ const SaveBtn = () => {
 
 const SaveBtnLink = (saveUrl) => {
   // selecting parent tag of save button
-  let linkparent = document.getElementById("generated");
+  let linkparent = document.getElementById("SaveBtn");
 
   // remove existed anchor tags
   let existedanchor = linkparent.getElementsByTagName("a");
@@ -86,24 +88,9 @@ const SaveBtnLink = (saveUrl) => {
   linkparent.appendChild(link);
 };
 
-button.addEventListener("click", submitting);
+Gnbtn.addEventListener("click", submitting);
 
-// JS Code of event for dark mode toggle
+// // JS Code for dark mode toggle
 toggle.addEventListener("change", (e) => {
-  document.body.classList.toggle("dark", e.target.checked);
+  document.body.classList.toggle("light", e.target.checked);
 });
-
-// Check if dark mode is enabled on page load
-if (localStorage.getItem("isDarkMode") === "true") {
-  toggle.checked = true;
-  document.documentElement.classList.add("dark");
-} else if (localStorage.getItem("isDarkMode") === "false") {
-  toggle.checked = false;
-  document.documentElement.classList.remove("dark");
-} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  toggle.checked = true;
-  document.documentElement.classList.add("dark");
-} else {
-  toggle.checked = false;
-  document.documentElement.classList.remove("dark");
-}
